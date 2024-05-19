4afe159a8cc9eb61179c29f5b1ad1c64f7f93c1b
KIRILL-SMOLIN <kirill-smol@mail.ru>
2024-05-19 23:32:47 +0500

    Add files via upload

---
+++ pract4.py
@@ -0,0 +1,7 @@
+    # Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"
+cars_mod = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars_mod)):
+    print('Я езжу на автомабиле марки', cars_mod[i])
+    cars_count += 10
+print(cars_count)
